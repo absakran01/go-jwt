db3e6f34c78059ed989b387b215479b131a846cd
absakran01 <146729719+absakran01@users.noreply.github.com>
2026-06-10 10:30:42 +0300

    Update JWT implementation to use environment variable for secret key and add token validation function

--- utils/token.go
+++ utils/token.go
@@ -1,5 +1,33 @@
 package utils
 
+import (
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = []byte(os.Getenv("JWT_SECRET")) 
+
 func GenerateToken(userID uint) (string, error) {
-	jwtSecret := []byte("your_secret_key") // In production, use an environment variable
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+	})
+
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func ValidateToken(tokenString string) (bool, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+	if err != nil || !token.Valid {
+		return false, err
+	}
+	return token.Valid, nil
 }
